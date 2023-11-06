ef908275e59fa763779c9d04dadd78a3a47ae4af
Henrique Barucco <henrique_barucco@hotmail.com>
2023-11-06 09:24:36 -0300

    fix(Address.java): adiciona implementação Serializable à classe Address fix(Contact.java): adiciona implementação Serializable à classe Contact
    A implementação da interface Serializable foi adicionada às classes Address e Contact para permitir que objetos dessas classes sejam serializados e desserializados, o que é útil em cenários como armazenamento em cache ou transferência de objetos pela rede.

--- src/main/java/com/got/vamosajudar/entities/dao/Address.java
+++ src/main/java/com/got/vamosajudar/entities/dao/Address.java
@@ -2,6 +2,8 @@ package com.got.vamosajudar.entities.dao;
 
 import com.got.vamosajudar.controllers.ong.dto.AddressDto;
 
+import java.io.Serializable;
+
 public record Address(
         String street,
         String number,
@@ -9,7 +11,7 @@ public record Address(
         String state,
         String postalCode,
         String country
-) {
+) implements Serializable {
     public Address(AddressDto address) {
         this(
                 address.getStreet(),
--- src/main/java/com/got/vamosajudar/entities/dao/Contact.java
+++ src/main/java/com/got/vamosajudar/entities/dao/Contact.java
@@ -2,11 +2,13 @@ package com.got.vamosajudar.entities.dao;
 
 import com.got.vamosajudar.controllers.ong.dto.ContactDto;
 
+import java.io.Serializable;
+
 public record Contact(
         String email,
         String phone,
         String website
-) {
+) implements Serializable {
     public Contact(ContactDto contact) {
         this(
                 contact.getEmail(),
